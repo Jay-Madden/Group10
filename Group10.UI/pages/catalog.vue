ab819e16dce55edb11992d2ea0565ed4edc8a88d
Jay <jaymaddencox@gmail.com>
2020-11-17 18:33:43 -0500

    Catalog route addition

--- Group10.UI/pages/catalog.vue
+++ Group10.UI/pages/catalog.vue
@@ -5,24 +5,38 @@
         ><v-card-title> Etsy Catalog </v-card-title>
       </v-card>
     </v-layout>
-
     <v-card class="ma-4" elevation="2" outlined>
+      <p v-if="$fetchState.pending">
+        <v-progress-circular indeterminate />
+      </p>
+      <p v-else-if="$fetchState.error">An error occurred :(</p>
       <v-list class="overflow-y-auto">
         <v-list-item v-for="(item, i) in items" :key="i">
-          <v-row>
-            <v-col align-self="center">
-              <b>{{ i }}:</b>
-            </v-col>
-            <v-col align-self="center">
-              {{ item.name }}
-            </v-col>
-            <v-col align-self="center">
-              {{ item.description }}
-            </v-col>
-            <v-card class="ma-2">
-              <v-col> ${{ item.cost }} </v-col>
-            </v-card>
-          </v-row>
+          <v-hover v-slot="{ hover }">
+            <v-row>
+              <v-col cols="auto" align-self="center">
+                <b>{{ i }}:</b>
+              </v-col>
+              <v-col align-self="center">
+                <v-card class="ma-4" elevation="3">
+                  <v-img
+                    :max-height="hover ? 150 : 50"
+                    :src="item.url_170x135"
+                    :class="{ 'on-hover': hover }"
+                  ></v-img>
+                </v-card>
+              </v-col>
+              <v-col align-self="center">
+                {{ item.title }}
+              </v-col>
+              <v-col align-self="center">
+                {{ item.description }}
+              </v-col>
+              <v-card class="ma-2">
+                <v-col> ${{ item.price }} </v-col>
+              </v-card>
+            </v-row>
+          </v-hover>
         </v-list-item>
       </v-list>
     </v-card>
@@ -33,12 +47,11 @@
 export default {
   data() {
     return {
-      items: Array.from({ length: 30 }, () => ({
-        name: 'ItemName',
-        cost: '12',
-        description: 'This is an item',
-      })),
+      items: [],
     };
+  },
+  async fetch() {
+    this.items = await fetch('api/catalog/info').then(res => res.json());
   },
 };
 </script>
